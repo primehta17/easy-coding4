167455fed28e9f5288b60bb1c929785593035af5
Priyanka Mehta <primehta17@gmail.com>
2025-04-15 21:13:17 +0530

    hour glass pattern

---
+++ five.js
@@ -0,0 +1,31 @@
+//watchglass pattern 
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+let nums=5,star="";
+for(let i=1;i<=nums;i++){
+    for(let j=1;j<=i;j++){
+        star+=" ";
+    }
+    for(let j=i;j<=nums;j++){
+        star+="* ";
+    }
+    star +="\n";
+}
+for(let i=2;i<=nums;i++){
+    for(let j=i;j<=nums;j++){
+        star+=" ";
+    }
+    for(let j=1;j<=i;j++){
+        star+="* ";
+    }
+    star +="\n";
+}
+console.log(star);

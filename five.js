0ed52fb3c8d131aed3bea68837b06bae4112678a
Priyanka Mehta <primehta17@gmail.com>
2025-04-15 21:20:27 +0530

    butterfly pattern

--- five.js
+++ five.js
@@ -1,13 +1,13 @@
 //watchglass pattern 
-// *********
-//  *******
-//   *****
-//    ***
-//     *
-//    ***
-//   *****
-//  *******
-// *********
+// * * * * * 
+// * * * * 
+//  * * * 
+//   * * 
+//    * 
+//   * * 
+//  * * * 
+// * * * * 
+// * * * * * 
 
 let nums=5,star="";
 for(let i=1;i<=nums;i++){
@@ -27,5 +27,50 @@ for(let i=2;i<=nums;i++){
         star+="* ";
     }
     star +="\n";
+}
+console.log(star);
+
+//butterfly pattern 
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+for(let i=1;i<=nums;i++){
+    for(let j=1;j<=i;j++){
+        star += "*";
+    }
+    for(let j=i;j<=nums;j++){
+        star += " ";
+    }
+    for(let j=i;j<nums;j++){
+        star += " ";
+    }
+    for(let j=1;j<=i;j++){
+        star += "*";
+    }
+      star+="\n"
+}
+for(let i=1;i<=nums;i++){
+    for(let j=i;j<=nums;j++){
+        star += "*";
+    }
+    for(let j=1;j<i;j++){
+        star += " ";
+    }
+    for(let j=1;j<=i;j++){
+        star += " ";
+    }
+    for(let j=i;j<=nums;j++){
+        star += "*";
+    }
+    star+="\n"
 }
 console.log(star);

2434cb95fc360479ea586ec4850bc644b6b9290e
Priyanka Mehta <primehta17@gmail.com>
2025-04-15 20:44:05 +0530

    origin main

---
+++ two.js
@@ -0,0 +1,22 @@
+//v pattern
+
+
+let nums=5, star="";
+for(let i=0;i<nums;i++){
+    for(let j=0;j<nums-1;j++){
+        if(i==j){
+            star+="* ";
+        }else{
+            star+=" ";
+        }
+    }
+    for(let j=0;j<nums;j++){
+        if(i+j==nums-1){
+            star+="* ";
+        }else{
+            star+=" ";
+        }
+    }
+    star +="\n";
+}
+console.log(star);
